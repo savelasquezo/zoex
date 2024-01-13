bec44c668b0ff87c9914398e1f964540a63ef3fe
savelasquezo <savelasquezo@gmail.com>
2024-01-13 17:41:25 -0500

    Zoex Deploy v1.9

--- core/core/os.py
+++ core/core/os.py
@@ -2,7 +2,7 @@ from .settings  import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://localhost:3000","https://zoexbet.com"]
+ALLOWED_HOSTS = ["http://localhost:3000","zoexbet.com","https://zoexbet.com"]
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000","https://zoexbet.com"]
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
