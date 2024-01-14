62d7dbb4a342bc8311cc7f3a4869a161e687ff43
savelasquezo <savelasquezo@gmail.com>
2024-01-13 22:02:13 -0500

    Zoex Deploy v2.2

--- public/src/app/layout.tsx
+++ public/src/app/layout.tsx
@@ -1,17 +1,17 @@
 import './globals.css'
-import type { Metadata } from 'next'
+import Head from 'next/head';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'ZoeX',
-  description: 'ZoeX Apuestas',
-}
-
 export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="es">
+      <Head>
+        <title>ZoeX</title>
+        <meta name="description" content="ZoeX Apuestas" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <body className={inter.className}>
           {children}
       </body>
