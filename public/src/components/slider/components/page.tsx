573a2fe9c7f710eb306bd1f08383b33f09740013
savelasquezo <savelasquezo@gmail.com>
2024-01-23 19:07:52 -0500

    Images Fix

--- public/src/components/slider/components/page.tsx
+++ public/src/components/slider/components/page.tsx
@@ -38,47 +38,38 @@ export const fetchImagenSliders = async () => {
 
 const Slider: React.FC<SliderProps> = ({ session  }) => {
   
-  const [imagenSliders, setImagenSliders] = useState<ImagenSlider[]>([]);
+    const [imagenSliders, setImagenSliders] = useState<ImagenSlider[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const storedImagenSliders = localStorage.getItem('imagenSliders');
-  
-        if (storedImagenSliders) {
-          const parsedImagenSliders = JSON.parse(storedImagenSliders) as ImagenSlider[];
-          setImagenSliders(parsedImagenSliders);
-        }
-  
-        const data = await fetchImagenSliders();
-        setImagenSliders(data);
-        localStorage.setItem('imagenSliders', JSON.stringify(data));
-      } catch (error) {
-        console.error('Error al obtener datos iniciales de imagenSliders:', error);
-      }
-    };
-  
-    fetchData();
-  }, []);
-  
+    useEffect(() => {
 
-  return (
-    <div className="w-screen mx-0">
-      <div id="default-carousel" className="relative" data-carousel="slide">
-          <div className={`overflow-hidden relative z-0 ${session ? 'h-[calc(100vh-112px)]' : 'h-56 md:h-[calc(100vh-56px)]'}`}>
-          <Suspense fallback={<p>...</p>}>
-              <Carousel slide={true} slideInterval={3000} indicators={false}
-                rightControl={<svg className="w-5 h-5 text-gray-400 sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>}
-                leftControl={<svg className="w-5 h-5 text-gray-400 sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>}>
-                {imagenSliders?.map((imagenSlider, i) => (
-                  <Image key={i} width={1340} height={500} src={imagenSlider.file} loader={imageLoader} priority={true} className="block w-full h-full" alt=""/>
-                ))}
-              </Carousel>
-          </Suspense>
-          </div>
+      fetchImagenSliders()
+        .then((data) => {
+          setImagenSliders(data);
+          localStorage.setItem('imagenSliders', JSON.stringify(data));
+        })
+        .catch((error) => {
+          console.error('Error al obtener datos iniciales de imagenSliders:', error);
+        });
+    },[]);
+
+    return (
+      <div className="w-screen mx-0">
+        <div id="default-carousel" className="relative" data-carousel="slide">
+            <div className={`overflow-hidden relative z-0 ${session ? 'h-[calc(100vh-112px)]' : 'h-56 md:h-[calc(100vh-56px)]'}`}>
+            <Suspense fallback={<p>...</p>}>
+                <Carousel slide={true} slideInterval={3000} indicators={false}
+                  rightControl={<svg className="w-5 h-5 text-gray-400 sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>}
+                  leftControl={<svg className="w-5 h-5 text-gray-400 sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>}>
+                  {imagenSliders?.map((imagenSlider, i) => (
+                    <Image key={i} width={1340} height={500} src={imagenSlider.file} loader={imageLoader} priority={true} className="block w-full h-full" alt=""/>
+                  ))}
+                </Carousel>
+            </Suspense>
+            </div>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Slider
+    );
+  };
+  
+  export default Slider
+  
