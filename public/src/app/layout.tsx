e23873e6678ac2533a65eaac8c045196bd69a11c
savelasquezo <savelasquezo@gmail.com>
2024-07-09 10:14:12 -0500

    Changes UI Lotterys + Google Analitics

--- public/src/app/layout.tsx
+++ public/src/app/layout.tsx
@@ -1,34 +1,41 @@
 import '@/styles/styles.css';
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Head from 'next/head';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'ZoeX',
   description: 'ZoeX',
-}
+};
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5GPWCDG30E"></script>
-        <script
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-5GPWCDG30E"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-5GPWCDG30E');
+              gtag('config', 'G-5GPWCDG30E', {
+                page_path: window.location.pathname,
+              });
             `,
           }}
         />
-      </Head>
+      </head>
       <body className={inter.className}>
-          {children}
+        {children}
       </body>
     </html>
-  )
+  );
 }
