ab34a30dc378ec3c02fc3a588355e3c209dcf5d4
savelasquezo <savelasquezo@gmail.com>
2024-08-19 17:18:13 -0500

    db-n

--- core/core/settings.py
+++ core/core/settings.py
@@ -148,7 +148,7 @@ ASGI_APPLICATION = "core.asgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "zoex",
+        "NAME": "dbpostgresql",
         "USER": "postgres",
         "PASSWORD": "4oPn2655Lmn",
         "HOST": "localhost",
