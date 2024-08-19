e38be0c6925d43a0e31eb6df7c77e8a6688edd3a
savelasquezo <savelasquezo@gmail.com>
2024-08-19 17:12:12 -0500

    LoadEnv Edit

--- core/core/settings.py
+++ core/core/settings.py
@@ -9,23 +9,23 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from pathlib import Path
-from datetime import timedelta
-
 import os
-import environ
+
+from dotenv import load_dotenv
+from datetime import timedelta
+from pathlib import Path
+
+# Environment variable patch / '.env'.
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Environment variable patch / '.env'.
-env = environ.Env()
-environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 CONFIRMO_KEY = os.getenv('CONFIRMO_KEY')
