f0a560e84254fcda33e2ba6b906331b2b0a0b3fa
savelasquezo <savelasquezo@gmail.com>
2024-08-19 17:34:24 -0500

    EditLogs

--- core/core/settings.py
+++ core/core/settings.py
@@ -229,6 +229,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+if not os.path.exists(os.path.join(BASE_DIR, 'static')):
+    os.makedirs(os.path.join(BASE_DIR, 'static'))
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
